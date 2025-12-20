36f3e56341421f0911ce2b3995858f0ba585ea1a
Developerkubilay <kubiss52@yahoo.com>
2025-12-20 14:46:57 +0300

    bug fix and i edited model (not finished)

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 #include <math.h>
 
 LiquidCrystal lcd(8, 9, 13, 12, 11, 10);
-
-const int stepsPerRevolution = 2048;
 // Pinler: IN1->4, IN2->3, IN3->2, IN4->A5
-Stepper myStepper(stepsPerRevolution, 5, 4, 3, 2);
+Stepper myStepper(2048, 5, 4, 3, 2);
 
 #define STEP 6
 #define DIR 7
+
+bool running = false;
 
 void setup()
 {
@@ -19,8 +19,10 @@ void setup()
     pinMode(STEP, OUTPUT);
     pinMode(DIR, OUTPUT);
     pinMode(A5, OUTPUT);
+    pinMode(A4, INPUT_PULLUP);
 
     digitalWrite(DIR, HIGH);
+    digitalWrite(A5, HIGH);
 
     myStepper.setSpeed(10);
 }
@@ -28,42 +30,50 @@ void setup()
 void loop()
 {
     static unsigned long lastTime = 0;
+    static float lastTemperatureC = NAN;
+
+    bool btnOn = (digitalRead(A4) == LOW);
 
     if (millis() - lastTime >= 1000)
     {
         lastTime = millis();
         int rawValue = analogRead(A0);
         float voltage = rawValue * (5.0 / 1023.0);
-        
+
         // NTC GND'ye, 100k Direnç 5V'a bağlı (Standart)
-        if (voltage > 4.9) voltage = 4.9;
-        if (voltage < 0.1) voltage = 0.1;
+        if (voltage > 4.9)
+            voltage = 4.9;
+        if (voltage < 0.1)
+            voltage = 0.1;
 
         float Rntc = 100000.0 * (5.0 - voltage) / voltage;
-        float temperatureC = 1.0 / ((1.0 / (25.0 + 273.15)) + (log(Rntc / 100000.0) / 3950.0)) - 273.15;
+        lastTemperatureC = 1.0 / ((1.0 / (25.0 + 273.15)) + (log(Rntc / 100000.0) / 3950.0)) - 273.15;
 
         lcd.clear();
         lcd.setCursor(0, 0);
         lcd.print("Sicaklik:");
         lcd.setCursor(0, 1);
-        lcd.print(temperatureC);
+        lcd.print(lastTemperatureC);
         lcd.print(" C");
     }
 
-    static unsigned long lastStepperTime = 0;
-    if (millis() - lastStepperTime >= 5)
+    bool shouldRun = btnOn && !isnan(lastTemperatureC) && (lastTemperatureC > 60);
+
+    if (shouldRun)
     {
-        lastStepperTime = millis();
-        myStepper.step(1);
-    }
-    /*
         digitalWrite(STEP, HIGH);
         delayMicroseconds(3000);
         digitalWrite(STEP, LOW);
         delayMicroseconds(3000);
+        myStepper.step(1);
+    }
+    else
+    {
+        if (running)
+        {
+            digitalWrite(STEP, LOW);
+        }
+    }
 
-        */
-
-    digitalWrite(A5, HIGH);
-
+    running = shouldRun;
 }
